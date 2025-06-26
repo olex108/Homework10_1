1a2cc98af95c324e9f1906c28e1bc715a963c208
Alex <oleksiymosiychuk108@gmail.com>
2025-06-27 00:18:10 +0300

    Update functions in processing.py

--- src/processing.py
+++ src/processing.py
@@ -1,14 +1,13 @@
-def filter_by_state(data_list: list, state='EXECUTED') -> list:
+def filter_by_state(data_list: list, state: str = "EXECUTED") -> list:
     """Функция принимает список словарей и опционально значение для ключа state
     возвращает новый список словарей, содержащий только те словари, у которых ключ соответствует указанному значению"""
 
-    new_data_list = []
-    return new_data_list
+    return [data for data in data_list if data["state"] == state]
 
-def sort_by_date(data_list: list, sort_value=None) -> list:
+
+def sort_by_date(data_list: list, reverse_parameter: bool = True) -> list:
     """Функция принимает список словарей и необязательный параметр,
     задающий порядок сортировки (по умолчанию — убывание)
     возвращает новый список, отсортированный по дате (date)"""
 
-    new_data_list = []
-    return new_data_list
+    return sorted(data_list, key=lambda data: data["date"], reverse=reverse_parameter)
