a2636aba301166a0e8496e1d73543e208d1427ee
Alex <oleksiymosiychuk@gmail.com>
2025-07-02 00:21:58 +0300

    generators.py init

---
+++ tests/test_generators.py
@@ -0,0 +1,13 @@
+import pytest
+
+# Test for function filter_by_currency
+def test_filter_by_currency():
+    pass
+
+# Test for function transaction_descriptions
+def test_transaction_descriptions():
+    pass
+
+# Test for function card_number_generator
+def test_card_number_generator():
+    pass
