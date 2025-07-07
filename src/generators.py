78b39fb77d2f3ff487a55c32bd16073e13b28626
Alex <oleksiymosiychuk@gmail.com>
2025-07-07 11:05:34 +0300

    generators add functions and tests

--- src/generators.py
+++ src/generators.py
@@ -1,80 +1,32 @@
-def filter_by_currency():
+import random
+
+
+def filter_by_currency(transactions: list[dict], currency: str) -> GeneratorExit[list]:
     """
     Функция принимает на вход список словарей, представляющих транзакции.
     Функция должна возвращать итератор, который поочередно выдает транзакции, где валюта операции соответствует
     заданной (например, USD)
-
-    usd_transactions = filter_by_currency(transactions, "USD")
-    for _ in range(2):
-        print(next(usd_transactions))
-
-    >>> {
-              "id": 939719570,
-              "state": "EXECUTED",
-              "date": "2018-06-30T02:08:58.425572",
-              "operationAmount": {
-                  "amount": "9824.07",
-                  "currency": {
-                      "name": "USD",
-                      "code": "USD"
-                  }
-              },
-              "description": "Перевод организации",
-              "from": "Счет 75106830613657916952",
-              "to": "Счет 11776614605963066702"
-          }
-          {
-                  "id": 142264268,
-                  "state": "EXECUTED",
-                  "date": "2019-04-04T23:20:05.206878",
-                  "operationAmount": {
-                      "amount": "79114.93",
-                      "currency": {
-                          "name": "USD",
-                          "code": "USD"
-                      }
-                  },
-                  "description": "Перевод со счета на счет",
-                  "from": "Счет 19708645243227258542",
-                  "to": "Счет 75651667383060284188"
-           }
     """
-    return
+    return (transaction for transaction in transactions if transaction["operationAmount"]["currency"]["code"] == currency)
 
-def transaction_descriptions():
+
+def transaction_descriptions(transactions: list[dict]):
     """
-    Генераторю, который принимает список словарей с транзакциями и возвращает описание каждой операции по очереди.
-
-    Пример использования функции
-    descriptions = transaction_descriptions(transactions)
-    for _ in range(5):
-        print(next(descriptions))
-
-    >>> Перевод организации
-        Перевод со счета на счет
-        Перевод со счета на счет
-        Перевод с карты на карту
-        Перевод организации
+    Генератор, который принимает список словарей с транзакциями и возвращает описание каждой операции по очереди.
     """
 
-    return
+    for transaction in transactions:
+        yield transaction["description"]
 
-def card_number_generator():
+
+def card_number_generator(start_number: int, stop_number: int):
     """
-    Генератор, который выдает номера банковских карт в формате XXXX XXXX XXXX XXXX, где X  — цифра номера карты.
+    Генератор, который выдает номера банковских карт в формате XXXX XXXX XXXX XXXX, где X — цифра номера карты.
     Генератор может сгенерировать номера карт в заданном диапазоне от 0000 0000 0000 0001 до 9999 9999 9999 9999.
     Генератор должен принимать начальное и конечное значения для генерации диапазона номеров.
-
-
-    Пример использования функции
-    for card_number in card_number_generator(1, 5):
-        print(card_number)
-
-    >>> 0000 0000 0000 0001
-        0000 0000 0000 0002
-        0000 0000 0000 0003
-        0000 0000 0000 0004
-        0000 0000 0000 0005
     """
 
-    return
+    for number in range(start_number, stop_number + 1):
+        # number = str(random.randint(start_number, stop_number))
+        number = f"{'0' * (16-len(str(number)))}{str(number)}"
+        yield f"{str(number)[0:4]} {str(number)[4:8]} {str(number)[8:12]} {str(number)[12:16]}"
--- tests/test_generators.py
+++ tests/test_generators.py
@@ -1,13 +1,41 @@
+from unittest import expectedFailure
+
 import pytest
 
+from src.generators import filter_by_currency, transaction_descriptions, card_number_generator
+
+
 # Test for function filter_by_currency
-def test_filter_by_currency():
-    pass
+def test_filter_by_currency(list_of_transactions) -> None:
+    expected_result = [
+        {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572', 'operationAmount': {'amount': '9824.07', 'currency': {'name': 'USD', 'code': 'USD'}}, 'description': 'Перевод организации', 'from': 'Счет 75106830613657916952', 'to': 'Счет 11776614605963066702'},
+        {'id': 142264268, 'state': 'EXECUTED', 'date': '2019-04-04T23:20:05.206878',
+         'operationAmount': {'amount': '79114.93', 'currency': {'name': 'USD', 'code': 'USD'}},
+         'description': 'Перевод со счета на счет', 'from': 'Счет 19708645243227258542',
+         'to': 'Счет 75651667383060284188'},
+        {'id': 895315941, 'state': 'EXECUTED', 'date': '2018-08-19T04:27:37.904916',
+         'operationAmount': {'amount': '56883.54', 'currency': {'name': 'USD', 'code': 'USD'}},
+         'description': 'Перевод с карты на карту', 'from': 'Visa Classic 6831982476737658',
+         'to': 'Visa Platinum 8990922113665229'}
+    ]
+    iterator = filter_by_currency(list_of_transactions, "USD")
+    result = list(iterator)
+    assert result == expected_result
+
 
 # Test for function transaction_descriptions
-def test_transaction_descriptions():
-    pass
+def test_transaction_descriptions(list_of_transactions) -> None:
+    generator = transaction_descriptions(list_of_transactions)
+    assert next(generator) == "Перевод организации"
+    assert next(generator) == "Перевод со счета на счет"
+    assert next(generator) == "Перевод со счета на счет"
+
+
 
 # Test for function card_number_generator
-def test_card_number_generator():
-    pass
+def test_card_number_generator() -> None:
+    generator = card_number_generator(1, 5)
+    assert next(generator) == "0000 0000 0000 0001"
+    assert next(generator) == "0000 0000 0000 0002"
+    assert next(generator) == "0000 0000 0000 0003"
+
