2879b49a30483f0cd494db59123aa811fb40db24
Alex <oleksiymosiychuk@gmail.com>
2025-07-22 11:14:22 +0300

    update test_utils.py add index.html

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,5 +1,5 @@
 import json
-from unittest.mock import Mock
+from unittest.mock import Mock, patch
 
 from src.utils import get_transactions_data
 
@@ -8,17 +8,17 @@ def test_get_transactions_data() -> None:
 
     # Тестирование по заданным данным в файле
     mock_data = Mock(return_value=[{"key": "value"}])
-    json.load = mock_data
-    assert get_transactions_data("data/operations.json") == [{"key": "value"}]
+    with patch('json.load', mock_data):
+        assert get_transactions_data("data/operations.json") == [{"key": "value"}]
 
     # Тестирование по некорректных данных в файле
     mock_data_dict = Mock(return_value={"key": "value"})
-    json.load = mock_data_dict
-    assert get_transactions_data("data/operations.json") == []
+    with patch('json.load', mock_data_dict):
+        assert get_transactions_data("data/operations.json") == []
 
     mock_data_empty = Mock(return_value=None)
-    json.load = mock_data_empty
-    assert get_transactions_data("data/operations.json") == []
+    with patch('json.load', mock_data_empty):
+        assert get_transactions_data("data/operations.json") == []
 
     # Тестирование функции при вызове несуществующего файла
     assert get_transactions_data("data/operation.json") == []
